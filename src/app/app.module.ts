08056cda04b57bf2dec08fed259ad62316588a3b
mjaiswara <manoj1.kss@gmail.com>
2024-11-17 19:08:03 +0530

    first draft of imutics project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './component/header/header.component';
+import { BannerComponent } from './component/banner/banner.component';
+import { AboutComponent } from './component/about/about.component';
+import { ContactComponent } from './component/contact/contact.component';
+import { TeamsComponent } from './component/teams/teams.component';
+import { ServicesComponent } from './component/services/services.component';
+import { GalleryComponent } from './component/gallery/gallery.component';
+import { TestimonialComponent } from './component/testimonial/testimonial.component';
+import { FooterComponent } from './component/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BannerComponent,
+    AboutComponent,
+    ContactComponent,
+    TeamsComponent,
+    ServicesComponent,
+    GalleryComponent,
+    TestimonialComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
